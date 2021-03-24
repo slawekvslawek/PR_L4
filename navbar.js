3463b43cb792a5d16d665ee9a7f18624a77ca0eb
Slawek <twitczu4321@gmail.com>
2021-03-24 19:13:47 +0100

    quiz

--- navbar.js
+++
@@ -1,5 +0,0 @@
-let nav = '<ul><li><a href="index.html">Strona główna</a</li>'+
-    '<li><a href="kontakt.html">Kontakt</a></li></ul>';
-
-let navElem = document.querySelector(".navbar");
-navElem.innerHTML = nav;
